07b5731ce6a7c88cc5d93c6de30ec4363c98921b
DanilenkoDanil <69428366+DanilenkoDanil@users.noreply.github.com>
2023-01-20 01:47:26 +0200

    Add files via upload

--- track.py
+++ track.py
@@ -43,15 +43,11 @@ def main():
             time.sleep(2)
             continue
         print(current_symbols)
-
-        symbol_info = db.get_symbol(order['symbol'])
-        target = get_active_target([symbol_info[0], symbol_info[1], symbol_info[2]])
-        if target == 0:
-            db.delete_symbol(order['symbol'])
-            continue
-
-        if time.time() - float(symbol_info[3]) > 60*60*24*10:
-            db.delete_symbol(order['symbol'])
+        try:
+            symbol_info = db.get_symbol(order['symbol'])
+            target = get_active_target([symbol_info[0], symbol_info[1], symbol_info[2]])
+        except Exception as e:
+            print(e)
             continue
         print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         print(symbol_info[5])
@@ -70,7 +66,7 @@ def main():
                          f'&percent={percent}'
                          f'&type=long'
             )
-            db.close_target(order['symbol'], target[0])
+            db.delete_symbol(order['symbol'])
         else:
             print('up!!!!')
             percent = 1 - round(float(order['lastPrice'])/float(symbol_info[4]) * 100 + 100, 2)
@@ -84,7 +80,7 @@ def main():
                          f'&percent={percent}'
                          f'&type=short'
                          )
-            db.close_target(order["symbol"], target[0])
+            db.delete_symbol(order['symbol'])
 
 
 if __name__ == "__main__":
